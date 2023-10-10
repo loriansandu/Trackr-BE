f941ad9848a08db1af6dfc0c68499f8bacee4041
lorian-andreisandu <loriansandu@gmail.com>
2023-10-10 11:12:35 +0300

    -update

---
+++ src/test/java/com/sandu/roomrental/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandu.roomrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
